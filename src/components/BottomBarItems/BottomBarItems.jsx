01bc2aadc2edd98c39a3c0b676d59761e164c137
Pedro Amaro <148393438+Amaro-peter@users.noreply.github.com>
2024-09-09 19:59:49 -0300

    Adjust gap of Icons in the BottomBar

--- src/components/BottomBarItems/BottomBarItems.jsx
+++ src/components/BottomBarItems/BottomBarItems.jsx
@@ -3,15 +3,25 @@ import Notifications from "../NavBarItems/Notifications"
 import Projetos from "../NavBarItems/Projetos"
 import Search from "../NavBarItems/Search"
 import LogOut from "../NavBarItems/LogOut"
+import { Flex } from "@chakra-ui/react"
 
 function BottomBarItems( {sizeOfIcon, sizeOfText} ) {
   return (
     <>
-        <Home sizeOfIcon={sizeOfIcon} sizeOfText={sizeOfText} />
-        <Search sizeOfIcon={sizeOfIcon} sizeOfText={sizeOfText} />
-        <Projetos sizeOfIcon={sizeOfIcon} sizeOfText={sizeOfText} />
-        <Notifications sizeOfIcon={sizeOfIcon} sizeOfText={sizeOfText} />
-        <LogOut sizeOfIcon={sizeOfIcon} sizeOfText={sizeOfText} />     
+        <Flex
+        direction={"row"}
+        gap={5}
+        w={"full"}
+        h={"full"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        >
+          <Home sizeOfIcon={sizeOfIcon} sizeOfText={sizeOfText} />
+          <Search sizeOfIcon={sizeOfIcon} sizeOfText={sizeOfText} />
+          <Projetos sizeOfIcon={sizeOfIcon} sizeOfText={sizeOfText} />
+          <Notifications sizeOfIcon={sizeOfIcon} sizeOfText={sizeOfText} />
+          <LogOut sizeOfIcon={sizeOfIcon} sizeOfText={sizeOfText} />   
+        </Flex>  
     </>
   )
 }
